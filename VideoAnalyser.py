85e8020db7df4eb79cf6113712a114431094fc29
ninjafrostpn <cst1g16@soton.ac.uk>
2019-12-08 01:51:06 +0000

    Show data from xlsx alongside the video view

--- VideoAnalyser.py
+++ VideoAnalyser.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np
+import pandas as pd
 import pygame
 from pygame.locals import *
 from scipy.ndimage.filters import convolve
-import time
+
+# Function for converting HH:MM:SS to total seconds
+timetosec = np.vectorize(lambda x: (((x.hour * 60) + x.minute) * 60) + x.second)
+
+# Read data in from the xlsx file as a pandas DataFrame (remove metadata in first 6 rows)
+df = pd.read_excel("SOES6008_coursework_DATA.xlsx", skiprows=6)
+df.columns = ["ActualTime", "VideoTime", "Lat", "Lon", "Dist", "Depth", "Temp", "Salinity"]
+df = df.assign(VideoTimeSecs=timetosec(df["VideoTime"]))
 
 # Initialise window
 pygame.init()
@@ -15,9 +23,11 @@ prevkeys = set()
 
 # Colours
 WHITE = np.int32([255] * 3)
+GREEN = np.int32([0, 255, 0])
+CYAN = np.int32([0, 255, 255])
 
 # Font for drawing text with
-textfont = pygame.font.Font(None, 50)
+textfont = pygame.font.Font(None, 30)
 
 # Initialise all window and video-getting tools
 cap = cv2.VideoCapture("IsisROV_dive148_TRANSECT.m4v")
@@ -25,9 +35,10 @@ capn = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 capfps = cap.get(cv2.CAP_PROP_FPS)
 capw, caph = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 capsize = np.int32([capw, caph])
-screensize = np.int32([capw, caph + 50])
+screensize = np.int32([capw + 300, caph])
 screen = pygame.display.set_mode(screensize)
-viewport = screen.subsurface([0, 50, capw, caph])
+viewport = screen.subsurface([0, 0, capw, caph])
+dataport = screen.subsurface([capw, 0, 300, caph])
 pygame.display.set_caption("VIDEO", "Video")
 print("{:.0f} frames ({:.0f} x {:.0f}) at {} FPS".format(capn, capw, caph, capfps))
 
@@ -35,7 +46,6 @@ print("{:.0f} frames ({:.0f} x {:.0f}) at {} FPS".format(capn, capw, caph, capfp
 def getnewframe(pos=None):
     if pos is not None:
         if pos < 0:
-            print(pos)
             pos = 0
         cap.set(cv2.CAP_PROP_POS_FRAMES, pos)
     pos = cap.get(cv2.CAP_PROP_POS_FRAMES)
@@ -45,6 +55,10 @@ def getnewframe(pos=None):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = np.rot90(np.fliplr(frame))
     return pos, frame
+
+
+def writedataline(text, lineno, col=WHITE):
+    dataport.blit(textfont.render(text, True, col), (20, 30 * lineno))
 
 
 # Blob-shaped filter used to find red blobs
@@ -60,20 +74,17 @@ while True:
     if K_SPACE in keys and K_SPACE not in prevkeys:
         pause = not pause
     if K_RIGHT in keys:
-        if not pause:
-            pos += 10 * skipspeed
-            pos, frame = getnewframe(pos)
-        elif K_RIGHT not in prevkeys:
+        if pause:
             pos += skipspeed
-            pos, frame = getnewframe(pos)
+        else:
+            pos += 10 * skipspeed
+        pos, frame = getnewframe(pos)
     elif K_LEFT in keys:
-        if not pause:
-            pos -= 10 * skipspeed
-            pos, frame = getnewframe(pos)
-        elif K_LEFT not in prevkeys:
-            print(pause)
+        if pause:
             pos -= skipspeed
-            pos, frame = getnewframe(pos)
+        else:
+            pos -= 10 * skipspeed
+        pos, frame = getnewframe(pos)
     elif not pause:
         # elif, not if, to provide allow skipping back to frame 0
         for i in range(skipspeed):
@@ -94,7 +105,27 @@ while True:
     pygame.draw.circle(viewport, WHITE, upperdot, 20, 1)
     pygame.draw.circle(viewport, WHITE, lowerdot, 20, 1)
     pygame.draw.line(viewport, WHITE, lowerdot, upperdot, 1)
-    screen.blit(textfont.render("Frame: {:07.0f}".format(pos), True, WHITE), (0, 0))
+    scalebar = np.linalg.norm(upperdot - lowerdot)
+    pygame.draw.rect(viewport, WHITE, [20, 20, scalebar, scalebar], 1)
+    viewport.blit(textfont.render("10 x 10", True, WHITE), [20, 20 + scalebar])
+    writedataline("~ VIDEO ~", 1)
+    writedataline("Frame: {:05.0f}/{:.0f}".format(pos, capn), 2)
+    writedataline("Time: {:04.0f}s/{:.0f}s".format(pos / capfps, capn / capfps), 3)
+    frametime = pos / capfps
+    if int(frametime) in df["VideoTimeSecs"]:
+        dataindex = np.argwhere(int(frametime) == df["VideoTimeSecs"])[0][0]
+        datatime = int(frametime)
+    else:
+        dataindex = np.searchsorted(df["VideoTimeSecs"], int(frametime)) - 1
+        datatime = df["VideoTimeSecs"][dataindex]
+    writedataline("~ DATA ~", 5, GREEN)
+    writedataline("Time: {:04.0f}s/{:.0f}s".format(datatime, max(df["VideoTimeSecs"])), 6, GREEN)
+    writedataline("Dist: {: 04.0f}.{:02.0f}m".format(df["Dist"][dataindex], abs(df["Dist"][dataindex] * 100) % 100),
+                  7, GREEN)
+    writedataline("Depth: {:.2f}m".format(df["Depth"][dataindex]), 8, GREEN)
+    writedataline("Temp: {:.4f}°C".format(df["Temp"][dataindex]), 9, GREEN)
+    writedataline("~ CALC ~", 11, CYAN)
+    writedataline("px per cm: {:.3f}".format(scalebar / 10), 12, CYAN)
     pygame.display.flip()
     prevkeys = keys.copy()
     for e in pygame.event.get():
